6ed98c3ffeeb76235c19219725ad6f31e1469324
Giovanni Montanari <giovannimontanari3@gmail.com>
2023-10-18 19:16:13 -0300

    create a welcome screen

--- onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding/welcome/WelcomeScreen.kt
@@ -10,13 +10,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.plcoding.core.R
+import com.plcoding.core_ui.LocalSpacing
 
 @Composable
 fun WelcomeScreen() {
+
+    val spacing = LocalSpacing.current
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -27,6 +32,6 @@ fun WelcomeScreen() {
             textAlign = TextAlign.Center,
             style = MaterialTheme.typography.h1
         )
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(spacing.spaceMedium))
     }
 }
