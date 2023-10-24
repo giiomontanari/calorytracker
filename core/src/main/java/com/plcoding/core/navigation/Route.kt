6bb4b38db08ddb17dd67c47ce8b4dbac8ea86839
Giovanni Montanari <giovannimontanari3@gmail.com>
2023-10-24 10:30:27 -0300

    create navigation

---
+++ core/src/main/java/com/plcoding/core/navigation/Route.kt
@@ -0,0 +1,15 @@
+package com.plcoding.core.navigation
+
+object Route {
+    const val WELCOME = "welcome"
+    const val AGE = "age"
+    const val GENDER = "gender"
+    const val HEIGHT = "height"
+    const val WEIGHT = "weight"
+    const val NUTRIENT_GLOBAL = "nutrient_global"
+    const val ACTIVITY = "activity"
+    const val GOAL = "goal"
+
+    const val TRACKER_OVERVIEW = "tracker_overview"
+    const val SEARCH = "search"
+}
