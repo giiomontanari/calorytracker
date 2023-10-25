9a477cd9885d8f5563272d5acea1c716ebec73e8
Giovanni Montanari <giovannimontanari3@gmail.com>
2023-10-25 12:41:08 -0300

    create a genderScren

---
+++ onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding_presentation/gender/GenderScreen.kt
@@ -0,0 +1,19 @@
+package com.plcoding.onboarding_presentation.gender
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.plcoding.core.util.UiEvent
+import com.plcoding.core_ui.LocalSpacing
+
+@Composable
+fun GenderScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit,
+    viewModel: GenderViewModel = hiltViewModel()
+) {
+    val spacing = LocalSpacing.current
+    LaunchedEffect(key1 = true) {
+
+    }
+    
+}
